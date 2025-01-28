a3791d57feebbf5ad55605604fd38482ad19c43e
Caleb Stein <caleb@calebstein.net>
2025-01-27 22:45:43 -0800

    add appbar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[200],
+      appBar: AppBar(
+        title: Text(
+          "To Do",
+          style: TextStyle(
+            color: Colors.grey[100],
+          )
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView(
+        children: [
+          ToDoTile()
+        ]
+      )
+    );
+  }
+}
